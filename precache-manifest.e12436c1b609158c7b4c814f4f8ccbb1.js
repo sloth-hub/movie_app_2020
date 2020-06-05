a3179945402de98fdb60debe10fd258feda2a179
sloth-hub <madamhh@gmail.com>
2020-06-05 19:45:16 +0900

    Updates

---
+++ precache-manifest.e12436c1b609158c7b4c814f4f8ccbb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99a985ac351329323a85bd4ba9d8427",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "74cfbb67e8e4ea8a27d2",
+    "url": "/movie_app_2020/static/css/main.11e847d2.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74cfbb67e8e4ea8a27d2",
+    "url": "/movie_app_2020/static/js/main.ad44b17c.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
